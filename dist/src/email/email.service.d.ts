a9380b2518b41e5f1b42a184191c8ad09f30c5a0
dietchse <wsuhandiet@gmail.com>
2026-06-04 00:26:34 +0700

    chore: Untrack dist folder and add to .gitignore

--- dist/src/email/email.service.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class EmailService {
-    private readonly logger;
-    private transporter;
-    constructor();
-    sendOrderReceipt(toEmail: string, orderNumber: string, customerName: string, trackingLink: string, tenantName: string): Promise<boolean>;
-}
--- dist/src/pages/entities/page.entity.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Page = void 0;
-class Page {
-}
-exports.Page = Page;
-//# sourceMappingURL=page.entity.js.map
