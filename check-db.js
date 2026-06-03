51222da9dc43770a0367033c56a1f610fd2d5400
dietchse <wsuhandiet@gmail.com>
2026-06-03 09:06:59 +0700

    fix: instantiate PrismaClient with adapter in check-db.js

--- check-db.js
+++ check-db.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const { PrismaClient } = require('@prisma/client');
+const { Pool } = require('pg');
+const { PrismaPg } = require('@prisma/adapter-pg');
 const bcrypt = require('bcryptjs');
 
-const prisma = new PrismaClient();
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
   try {
